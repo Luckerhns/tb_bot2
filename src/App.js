a9214b2d58b56a5deef2dde6a322ee5ad1bac4e4
Luckerhns <Luckerhackerr@gmail.com>
2022-12-09 20:41:02 +0300

    new button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { useEffect } from "react";
 
-const tg = window.Telegram.WebApp;
-
 const App = () => {
   useEffect((e) => {
     tg.ready();
   }, []);
 
-  const onClose = () => tg.close();
-  return (
-    <div className="App">
-      work
-      <button onClick={onClose}>Close</button>
-    </div>
-  );
+  return <div className="App">work</div>;
 };
 
 export default App;
