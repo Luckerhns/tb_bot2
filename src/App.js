7c240922f2875203ab871a4283c19bb546c38a40
Luckerhns <Luckerhackerr@gmail.com>
2022-12-09 21:26:06 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useEffect } from "react";
+import { useTelegram } from "./components/hooks/useTelegram";
+const tg = window.Telegram.WebApp;
 
 const App = () => {
+  const { onToggleButton, tg } = useTelegram();
+
   useEffect((e) => {
     tg.ready();
   }, []);
 
-  return <div className="App">work</div>;
+  return (
+    <div className="App" onClick={onToggleButton}>
+      toggle
+    </div>
+  );
 };
 
 export default App;
